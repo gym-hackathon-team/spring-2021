fd41d34420ee42bbc8cd8cd7f625c429b0de0b1b
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-03-05 01:49:51 +0300

    React + TypeScript

---
+++ resources/js/components/Example.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Example = () => {
+    let foo: string = "React";
+    const bar: string = "TypeScript";
+
+    return (
+        <h1>
+            Hello {foo} + {bar}
+        </h1>
+    );
+};
+
+export default Example;
