69d7cc81bff18eba99c6cd0c92ebeb5a9a468ddc
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-03-05 14:11:22 +0300

    Frontend structure edit

---
+++ resources/js/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Example from "./Example";
+
+function App() {
+    return (
+        <div className="App">
+            <div className="container">
+                <div>Hello there</div>
+                <Example />
+            </div>
+        </div>
+    );
+}
+
+export default App;
