c751db8e7f1c1564e8f9d10758e60e4dc09ceacc
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-03-09 12:22:12 +0300

    Remove excess method from AuthLoginRequest

--- app/Http/Requests/Auth/AuthLoginRequest.php
+++ app/Http/Requests/Auth/AuthLoginRequest.php
@@ -18,21 +18,4 @@ class AuthLoginRequest extends FormRequest
             'password' => 'required|string|min:6|max:20',
         ];
     }
-
-    /**
-     * Handle a failed validation attempt.
-     *
-     * @param  \Illuminate\Contracts\Validation\Validator $validator
-     * @return void
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    protected function failedValidation(Validator $validator)
-    {
-        $errors = (new ValidationException($validator))->errors();
-
-        throw new HttpResponseException(
-            response()->json(['errors' => $errors], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
-        );
-    }
 }
