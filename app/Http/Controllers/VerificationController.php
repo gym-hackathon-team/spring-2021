1117a4e246c6ac91b8f48f52f525870846a7e70c
CunningBird <68229355+CunningBird@users.noreply.github.com>
2021-03-11 16:24:01 +0300

    Verify email controller rebuild

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Verification\VerificationVerifyRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\Request;
@@ -13,8 +12,20 @@ class VerificationController extends Controller
         return response( ['message' => 'Email not verified'] );
     }
 
-    public function verify(VerificationVerifyRequest $request) {
+    public function verify(Request $request) {
         $user = User::find($request->route('id'));
+
+        if ( ! $user ) {
+            return response( [ 'message' => 'User not found' ] );
+        }
+
+        if (! hash_equals((string) $request->route('id'), (string) $user->getKey())) {
+            return response( [ 'message' => 'Invalid/Expired url provided' ] );
+        }
+
+        if (! hash_equals((string) $request->route('hash'), sha1($user->getEmailForVerification()))) {
+            return response( [ 'message' => 'Invalid/Expired url provided' ] );
+        }
 
         if ( $user->hasVerifiedEmail() ) {
             return response( [ 'message' => 'Email already verified' ] );
